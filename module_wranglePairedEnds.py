23de3c76adb3b9db582eacb04a4a083008760338
sungshine <sungbin401@gmail.com>
2015-03-03 18:21:36 -0500

    Removed hard-coded directory paths. Added STDIN system arguments for user to specify in/out directories. Added usage statement

--- module_wranglePairedEnds.py
+++ module_wranglePairedEnds.py
@@ -1,10 +1,12 @@
 __author__ = 'Sung Im'
 #!/usr/bin/env python
 import os
+import sys
 
 # Returns a dictionary of 'key : value' pairs.
 # Keys return the isolate identification numbers.
 # Values return the file path(s) to the raw read file(s).
+# Usage: module_wranglePairedEnds.py </path/to/input files/>
 
 def wranglePairedEnds(paths):
     for file in paths:
@@ -19,6 +21,7 @@ def wranglePairedEnds(paths):
             fileHash[newfile].append(file)
     return
 
+inputDirectory = sys.argv[1]
 paths = [os.path.join(inputDirectory,fn) for fn in next(os.walk(inputDirectory))[2]]    #store file path(s) in list
 
 fileHash = {}       #initiate file hash
