d9bfb6ad8a6be19b29f8bde6790740284bb22527
sungshine <sungbin401@gmail.com>
2015-03-03 18:18:03 -0500

    Removed hardcoded directory paths. STDIN is used for absolute in/out directory & file paths.

--- module_ABySS.py
+++ module_ABySS.py
@@ -6,6 +6,7 @@ import re
 
 # Generates hash of trimmed reads and invokes either SE or PE ABySS de novo assembly.
 # Outputs contig file(s) in FASTA format in user-specified directory.
+# Usage: module_ABySS.py </path/to/inputfiles/> </path/to/output directory/>
 
 def matchPE(prinseqPaths):              #use case hash generator for ABySS suffix requirement for PE reads.
     for file in prinseqPaths:
@@ -23,25 +24,25 @@ def matchPE(prinseqPaths):              #use case hash generator for ABySS suffi
 def moduleAbyssSE(inputfileOne):
     base = re.match("^(.*)_", inputfileOne)
     filename = base.group(1)
-    os.system("ABYSS "+"-k 21 "+ODprinseq+inputfileOne+" -o "+ODabyss+filename+".fa")
+    os.system("ABYSS "+"-k 21 "+inputDirectory+inputfileOne+" -o "+outputDirectory+filename+".fa")
 
 def moduleAbyssPE(inputfileOne, inputfileTwo):
     base = re.match("^(.*)_", inputfileTwo)
     filename = base.group(1)
-    os.system("cd "+ODabyss)
-    os.system("abyss-pe "+" K=21 "+"name="+ODabyss.file+".fa "+"in="+ODabyss+inputfileOne+" "+ODabyss+inputfileTwo)
+    os.system("cd "+outputDirectory)
+    os.system("abyss-pe "+" K=21 "+"name="+outputDirectory+filename+".fa "+"in="+outputDirectory+inputfileOne+" "+outputDirectory+inputfileTwo)
 
 
 ########################################################################################################################
 ###                                                   MAIN PROGRAM                                                   ###
 ########################################################################################################################
 
-ODprinseq = sys.argv[1]     #point to directory of trimmed (prinseq) files.
-ODabyss = sys.argv[2]       #point to output directory where SPAdes assemblies will be stored.
+inputDirectory = sys.argv[1]        #point to directory of trimmed (prinseq) files.
+outputDirectory = sys.argv[2]       #point to output directory where ABySS assemblies will be stored.
 
 fileHash = {}
 
-prinseqPaths = [os.path.join(ODprinseq, fn) for fn in next(os.walk(ODprinseq))[2]]
+prinseqPaths = [os.path.join(inputDirectory, fn) for fn in next(os.walk(inputDirectory))[2]]
 
 matchPE(prinseqPaths)       #sorts the file-path(s)-values in hash
 
